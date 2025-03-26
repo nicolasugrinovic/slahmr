75a2ccb5a4f4e4e8c151e1d43cb3c121876ddb9a
nugrinovic <nugrinovic@iri.upc.edu>
2025-03-27 00:10:32 +0100

    updated run script

--- scripts/run_for _multiphys_expi.sh
+++ scripts/run_for _multiphys_expi.sh
@@ -1,6 +1,6 @@
 # get PHALP data
 python run_opt.py data=expi run_opt=False run_vis=False data.overwrite=False data.root=/home/nugrinovic/code/CVPR_2024/slahmr/videos/expi/acro1 data.seq="around-the-back1_cam20" data.seq_id="acro1"
 # get SLAHMR data
-python run_opt.py data=expi run_opt=True run_vis=True data.overwrite=False data.root=/home/nugrinovic/code/CVPR_2024/slahmr/videos/expi/acro1 data.seq="${action}_${cam}" data.seq_id="${subj}"
+python run_opt.py data=expi run_opt=True run_vis=True data.overwrite=False data.root=/home/nugrinovic/code/CVPR_2024/slahmr/videos/expi/acro1 data.seq="around-the-back1_cam20" data.seq_id="acro1"
 # generate the scene_dict.pkl for MultiPhys
 python run_opt_world.py data=expi run_opt=False run_vis=True data.root=/home/nugrinovic/code/CVPR_2024/slahmr/videos/expi/acro1 data.seq="around-the-back1_cam20" data.seq_id=acro1
